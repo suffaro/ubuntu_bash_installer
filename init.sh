1cdf053b1268ee020b73d694455a15a097ff783e
suffaro <denys.pisotskyi@gmail.com>
2023-07-30 16:33:51 +0300

    some minor updates

--- init.sh
+++ init.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
-function commandExec() {
+function execute() {
   sudo $1
+  if [ $? -eq 0 ]; then
+  echo "[+] Succeded!"
+  else
+  
+  fi 
 };
 
+function logs() {  
+  echo "[process] $1"
+}
+
+function logs() {
+
+}
+
 # update and upgrade 
-echo "Updating your system..."
+logs "Updating your system..."
 sudo apt update && sudo apt upgrade -y
 sleep 1
 echo "Starting app installation..."
@@ -24,9 +37,24 @@ echo "downloading and installing packages"
 
 # chrome, vscode
 
-# snap telegram
+sudo apt-get install wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gpg
 
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code # or code-insiders
+
+
+# snap telegram
+sudo snap install telegram-desktop
 
 # powerconfig for wifi
 
+# sudo nano /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
+sudo systemctl restart NetworkManager
+
+sleep 10
 
